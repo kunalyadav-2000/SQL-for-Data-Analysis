fb8ffe60e3170846220b8571ddc41baa2894faf8
Kunal yadav <ykunal2399@gmail.com>
2026-06-10 16:08:20 +0530

    Add files via upload

---
+++ 02_Databases_and_Tables/solution.sql
@@ -0,0 +1,5 @@
+-- Databases & Relational Databases – Solutions
+
+-- 1. A database centrally stores, organises, and protects business data so it can be reliably queried and updated by authorised users and applications.
+-- 2. If a business has 100,000 customers each with multiple orders, a spreadsheet would be extremely slow, prone to formula errors, and couldn't support simultaneous updates from multiple employees.
+-- 3. Separation avoids data duplication (normalisation), reduces update errors (you change a customer’s address only once), and allows independent management of each entity.
