93faf8a91f256d1b714cd08d0ca1d418eede8e98
Kunal yadav <ykunal2399@gmail.com>
2026-06-10 16:02:09 +0530

    Add files via upload

---
+++ 01_Introduction_to _SQL/exercise.sql
@@ -0,0 +1,5 @@
+-- Introduction to SQL – Exercises
+
+-- 1. Write down three real‑life examples where SQL is used in business.
+-- 2. In your own words, explain why SQL is called a declarative language.
+-- 3. What is the difference between SQL and a database like PostgreSQL?
